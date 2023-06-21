3ebb560a4da6839525f70e372145e39c2142cb11
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2023-06-21 16:18:06 -0400

    Adding display density

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DisplayDensity, DisplayDensityToken } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: DisplayDensityToken, useValue: { displayDensity: DisplayDensity.cosy } }],
+
 })
 export class AppComponent {
 }
